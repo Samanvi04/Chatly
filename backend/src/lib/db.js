15aa4c4f705b080afb4731a86f47d64036a4ad3e
Samanvi <4samanvi@gmail.com>
2026-01-28 10:32:31 +0530

    backend

---
+++ backend/src/lib/db.js
@@ -0,0 +1,11 @@
+import mongoose from "mongoose";
+
+export const connectDB = async () => {
+    try{
+        await mongoose.connect(process.env.MONGODB_URL);
+        console.log(`MongoDB connected : ${process.env.MONGODB_URL}`);
+    }catch (error){
+            console.log("MongoDB connection error:", error);
+            
+        }
+    }
